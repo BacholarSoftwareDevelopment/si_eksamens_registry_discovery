6fab8ff391bb6649b42e678be2b9f766b1d3d534
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-12-14 18:41:29 +0100

    Project setup

---
+++ src/main/java/dk/si/exam/registry_discovery/RegistryDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package dk.si.exam.registry_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/si/exam/registry_discovery/RegistryDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.si.exam.registry_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
